952d5f90ffd27475c2cb27dee05c654bfb1d37e7
Dennis Njogu <dennis.njogu@student.moringaschool.com>
2022-11-03 11:25:49 +0300

    finished assignment

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+--Mr Chocolate 
+
+INSERT INTO bears (id,name, age, sex, color, temperament,alive)
+VALUES(1,'Mr. Chocolate',14,'M','grey','german shepherd', 1),
+(2,'Rowdy',14,'M','white','german shepherd', 1),
+(3,'Tabitha',15,'F','black','collie border', 1),
+(4,'Sergeant Brown',15,'F','black','golden retriever', 1),
+(5,'Melissa',16,'M','brown','belgium malinois', 1),
+(6,'Grinch',16,'M','brown','belgium malinois', 0),
+(7,'Wendy',13,'F','brown','jack russell terrier', 0),
+(8,NULL,13,'F','brown','jack russell terrier', 0);
